a90382d25ea46c815056cf7e48651d8f554bf9cf
SupakornTha <36919622+SupakornTha@users.noreply.github.com>
2018-09-11 21:40:00 +0700

    def checkRow()

--- warehouse3.py
+++ warehouse3.py
@@ -48,6 +48,7 @@ class warehouse:
         if  (self.row[b][a].get(data1)==None)and (self.ware[int(position[0])][int(position[1:3])].get(position)==None):
             self.row[b][a][data1]=position
             self.ware[int(position[0])][int(position[1:3])][position]=data1
+
     def retrieve(self,string):
         r=int((ord(string[0]) % 65) / 5)
         b=int(string[1])
@@ -55,29 +56,61 @@ class warehouse:
         c = a.get(string)
         a.pop(string)
         self.ware[int(c[0])][int(c[1:3])].pop(c)
-        print(int(c)+101000)
+    def findposition(self, string1):
+        string =self.posi(string1)
+        if self.ware[int(string[0])][int(string[1:3])].get(string)==None:
+            print(self.ware[int(string[0])][int(string[1:3])])
+            return True
+        else:
+            return False
+    def checksort(self,list):
+        k=[]
+        for i in list:
+            a = self.string2code(i)
+            b = str(int(a) + 101000)
+            k.append(self.findposition(b))
+        if False in k:
+            return False
+        else:
+            return True
     def sort(self,x,y):
         u=[]
         for i in self.ware[x-1][y-1]:
             if bool(self.code(self.ware[x-1][y-1][i],self.rownum[x-1])==i)==False:
                 c= self.ware[x-1][y-1][i]
                 u.append(c)
-        for i in u:
-            self.retrieve(i)
-            a=self.string2code(i)
-            c=str(int(a[1:3])+1)
-            if len(c)==1:
-                c="0"+c
-            b=str(int(a[0])+1)+c+a[3:]
-            self.insert(i,b)
-
-    def find(self, string):
+        if self.checksort(u)==True:
+            for i in u:
+                a = self.string2code(i)
+                b=str(int(a)+101000)
+                self.retrieve(i)
+                self.insert(i,b)
+                return True
+        else:
+            return False
+    def find(self,string):
         r = int((ord(string[0]) % 65) / 5)
         b = int(string[1])
-        if self.row[b - 1][r].get(string) != None:
+        if self.row[b - 1][r].get(string)!=None:
             return True
         else:
             return False
 
+    def checkRow(self, x):
+        if self.ware[x][0] >= 100:
+            print("Slot is empty. Cannot retrieve the product. ")
+        elif self.ware[x][2] >= 100:
+            print("Slot is empty. Cannot retrieve the product. ")
+        elif self.ware[x][3] >= 100:
+            print("Slot is empty. Cannot retrieve the product. ")
+        elif self.ware[x][4] >= 175:
+            print("Slot is empty. Cannot retrieve the product. ")
+        elif self.ware[x][5] >= 8000:
+            print("Slot is empty. Cannot retrieve the product. ")
+
 w = warehouse()
 w.insert("A111","519099")
+w.insert("A181","519098")
+w.insert("A121","515099")
+w.insert("A171","502099")
+print(w.ware5)

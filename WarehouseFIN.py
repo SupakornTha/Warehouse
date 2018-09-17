fdafd87a955bcd004f27a45827293614b084d749
SupakornTha <36919622+SupakornTha@users.noreply.github.com>
2018-09-17 20:17:55 +0700

    FIN

--- WarehouseFIN.py
+++ WarehouseFIN.py
@@ -17,37 +17,26 @@ class warehouse:
         self.posit=[65,70,75,80,85]
         self.ware = [self.ware1,self.ware2,self.ware3,self.ware4,self.ware5]
         self.belt  = []
-    def code(self, string,row):
+    def string2code(self,string):
+        e = str(int((ord(string[0]) % 65) / 5))
+        f = self.rownum[int(e)]
         a = str(int((ord(string[0]) % 65) / 5))
-        b = str((int((ord(string[0]))+(int(string[1:])-1))%int(self.slotnum[int(a)])))
+        b = str((int((ord(string[0])) + (int(string[1:]) - 1)) % int(self.slotnum[int(a)])))
         c = int(a) + int(string[1]) + int(string[2]) + int(string[3])
-        d = str((c % row))
+        d = str((c % f))
         if len(d) == 1:
             d = "0" + d
-        if len(b)==2:
-            b="0"+ b
-        elif len(b) ==1:
-            b="00"+b
-        return (a+str(d)+str(b))
-    def posi(self,string):
-        a=str(int(string[0]))
-        if len(string[1:])==3:
-            return a + "0"+str(int(string[1]))+"0"+string[2:]
-        elif len(string[1:])==5:
-            b=str(int(string[1:3]))
-            if len(b)==1:
-                b="0"+ b
-            return a+b+string[3:]
-    def string2code(self,string):
-        c = int(self.code(string, 20)[0])
-        d = self.rownum[c]
-        product=self.code(string,d)
+        if len(b) == 2:
+            b = "0" + b
+        elif len(b) == 1:
+            b = "00" + b
+        product=(a+str(d)+str(b))
         return product
-    def insert(self,data1,position1):
+    def insert(self,data1,position):
         a=int((ord(data1[0]) % 65) / 5)
         b=int(data1[1])-1
-        position=self.posi(position1)
-        if  (self.row[b][a].get(data1)==None)and (self.ware[int(position[0])][int(position[1:3])].get(position)==None):
+        if  (self.row[b][a].get(data1)==None)\
+            and (self.ware[int(position[0])][int(position[1:3])].get(position)==None):
             self.row[b][a][data1]=position
             self.ware[int(position[0])][int(position[1:3])][position]=data1
     def retrieve(self,string):
@@ -57,9 +46,16 @@ class warehouse:
         c = a.get(string)
         a.pop(string)
         self.ware[int(c[0])][int(c[1:3])].pop(c)
-    def findposition(self, string1):
-        string =self.posi(string1)
-        if self.ware[int(string[0])][int(string[1:3])].get(string)!=None:
+    def findposition(self, string):
+        a = str(int(string[0]))
+        if len(string[1:]) == 3:
+            return a + "0" + str(int(string[1])) + "0" + string[2:]
+        elif len(string[1:]) == 5:
+            b = str(int(string[1:3]))
+            if len(b) == 1:
+                b = "0" + b
+        string1= a + b + string[3:]
+        if self.ware[int(string1[0])][int(string1[1:3])].get(string1)!=None:
             return False
         else:
             return True
@@ -76,7 +72,7 @@ class warehouse:
     def sort(self,x,y):
         u=[]
         for i in self.ware[x-1][y-1]:
-            if bool(self.code(self.ware[x-1][y-1][i],self.rownum[x-1])==i)==False:
+            if bool(self.ware[x-1][y-1][i]==i)==False:
                 c= self.ware[x-1][y-1][i]
                 u.append(c)
         if self.checksort(u)==True:
@@ -90,24 +86,16 @@ class warehouse:
         r = int((ord(string[0]) % 65) / 5)
         b = int(string[1])
         if self.row[b - 1][r].get(string)!=None:
-            return True
+            return self.row[b - 1][r].get(string)
         else:
             return False
-
     def checkname(self,string):
-        if 1<=int(string[1])<=5:
-            if 0<=int(string[2:])<=99:
-                if 97<=ord(string[0])<=121:
-                    return chr(ord(string[0])-32)+string[1:]
-                elif 65<=ord(string[0])<=89:
-                    return string
+        if 97<=ord(string[0])<=121:
+            return chr(ord(string[0])-32)+string[1:]
+        elif 65<=ord(string[0])<=89:
+            return string
         else:
             return False
-    def checkRow(self, x):
-        x=x-1
-        for i in range(self.rownum[x]):
-            if self.ware[x][i] >= self.slotnum[x]:
-                print("Slot is empty. Cannot retrieve the product. ")
     def checknumber_product(self,w):
         t=0
         for i in self.ware[w-1]:
@@ -117,14 +105,7 @@ class warehouse:
         w=""
         for i in self.ware[ware][row]:
             w+= str(self.ware[ware][row].get(i))+","
-
         return w[:len(w) - 1]
-    def findproduct(self, string):
-        r = int((ord(string[0]) % 65) / 5)
-        b = int(string[1])
-        return self.row[b - 1][r].get(string)
-
-
     def info(self):
         print (" 0XXXX:Retrieve a product id XXXX")
         print (" 1XXXX:Store a product id XXXX")
@@ -140,7 +121,7 @@ class warehouse:
         print (" To enter the position please enter 6 digit number ABC")
         print ("  A  represents a number of warehouse.It has a value of 1 to 5.")
         print ("  B  represents a number of row in warehouse.It has a value of 01 to 20.")
-        print ("  C  represents a number of slot.It has a value of 001 to 400.")
+        print ("  C  represents a number of slot.It has a value of 000 to 399.")
     def data(self):
         for i in range(len(self.ware)):
             print ("Warehouse "+str(i+1))
@@ -152,14 +133,12 @@ class warehouse:
                 else:
                     b=self.get_item(i,j)
                 print ("Product in row "+str(j+1)+": id "+b)
-
     def command(self):
         print("")
         a = input("Please Enter the Command:")
         if 4<len(a)<7:
-            if a[0] == "0" and len(a)==5 and (65<=ord(a[1])<=89 or 97<=ord(a[1])<=121) and 49<=ord(a[2])<=53 \
-                    and 48<=ord(a[3])<=57 and 48<=ord(a[4])<=57 :
-                if self.checkname(a[1:]) == False or len(a) != 5: #or self.find(a[2:]) == True:
+            if a[0] == "0" and len(a)==5 and (65<=ord(a[1])<=89 or 97<=ord(a[1])<=121) and 49<=ord(a[2])<=53 and 48<=ord(a[3])<=57 and 48<=ord(a[4])<=57 :
+                if self.checkname(a[1:]) == False or len(a) != 5:
                     print ("Invalid product id. Please check the product id again")
                     return self.command()
                 else:
@@ -171,7 +150,7 @@ class warehouse:
                         print ("Belt is full. Cannot retrieve the product ")
                         return self.command()
                     else:
-                        c = self.findproduct(b)
+                        c = self.find(b)
                         self.retrieve(b)
                         self.belt.append(b)
                         print ("-Moving from Belt to Warehouse 1")
@@ -192,9 +171,7 @@ class warehouse:
                         print ("-Place product id "+b+" on the belt")
                         print ("-Retrieving Successfully! ")
                         return self.command()
-
-            elif a[0] == "1" and len(a)==5 and (65<=ord(a[1])<=89 or 97<=ord(a[1])<=121) and 49<=ord(a[2])<=53 and 48<=ord(a[3])<=57 \
-                    and 48<=ord(a[4])<=57:
+            elif a[0] == "1" and len(a)==5 and (65<=ord(a[1])<=89 or 97<=ord(a[1])<=121) and 49<=ord(a[2])<=53 and 48<=ord(a[3])<=57 and 48<=ord(a[4])<=57:
                 if self.checkname(a[1:]) == False or len(a) != 5:
                     print ("Invalid product id. Please check the product id again")
                     return self.command()
@@ -226,8 +203,8 @@ class warehouse:
                         else:
                             print("Slot is occupied. Cannot store the product.")
                         return self.command()
-            elif a[0] == "2" and len(a)==5 and a[len(a)-2:]=="00" and 49<=ord(a[1])<=53 :
-                if int(a[1]) not in range(0, 6) or int(a[2:len(a) - 2]) > self.rownum[int(a[1]) - 1]:
+            elif a[0] == "2" and  a[len(a)-2:]=="00" and 49<=ord(a[1])<=53 and 48<=int(a[len(a)-3])<=57 and 48<=ord(a[2])<=57 :
+                if int(a[2:len(a) - 2]) > self.rownum[int(a[1]) - 1]:
                     print("Invalid command.The command must include 2XY00 while X is a warehouse number and Y is a row number")
                     return self.command()
                 else:
@@ -255,8 +232,7 @@ class warehouse:
             elif a=="40000":
                 self.data()
                 return self.command()
-            elif a[0] == "5" and len(a)==5  and (65<=ord(a[1])<=89 or 97<=ord(a[1])<=121) and 49<=ord(a[2])<=53 \
-                    and 48<=ord(a[3])<=57 and 48<=ord(a[4])<=57:
+            elif a[0] == "5" and len(a)==5  and (65<=ord(a[1])<=89 or 97<=ord(a[1])<=121) and 49<=ord(a[2])<=53 and 48<=ord(a[3])<=57 and 48<=ord(a[4])<=57:
                 if 65<=ord(a[1])<=89 or 97<=ord(a[1])<=121:
                     if self.checkname(a[1:])!=False:
                         b = self.checkname(a[1:])
@@ -264,7 +240,7 @@ class warehouse:
                             print ("Product not found.")
                             return self.command()
                         else:
-                            c=w.findproduct(b)
+                            c=w.find(b)
                             print("Found the product at Warehouse: "+str(int(c[0])+1)+" row: "+str(int(c[1:3])+1)+" slot: "+str(int(c[3:])))
                             print(self.command())
                 else:
@@ -274,7 +250,7 @@ class warehouse:
                 print ("Invalid command.")
                 return self.command()
         elif len(a)==11:
-            if a[0] == "9"  and (65<=ord(a[1])<=89 or 97<=ord(a[1])<=121) and 49<=ord(a[2])<=53 and 48<=ord(a[3])<=57 and 48<=ord(a[4])<=57:
+            if a[0] == "9"  and (65<=ord(a[1])<=89 or 97<=ord(a[1])<=121) and 49<=ord(a[2])<=53 and 48<=ord(a[6])<=50 and 48<=ord(a[7])<=57 and 48<=ord(a[8])<=51 and 48<=ord(a[9])<=57 and 48<=ord(a[10])<=57 and 48<=ord(a[3])<=57 and 48<=ord(a[4])<=57 and 49<=ord(a[5])<=53 and int(a[6:8])<=self.rownum[int(a[5])-1] and int(a[8:]) < self.slotnum[int(a[5])-1] :
                 if a[5] == "0":
                     print("Invalid command.")
                     return self.command()
@@ -292,7 +268,7 @@ class warehouse:
                     return self.command()
                 else:
                     b=self.checkname(a[1:5])
-                    d=self.posi(a[5:])
+                    d=a[5:]
                     e=str(int(d[1:3])-1)
                     if len(e)==1:
                         e="0"+e
@@ -309,11 +285,12 @@ class warehouse:
                             self.insert(b,c)
                         print ("Move product "+b+" to Warehouse: "+str(int(d[0]))+" row: "+str(int(d[1:3]))+" slot: "+str(int(d[3:])))
                         return self.command()
-
+            else:
+                print("Invalid command.")
+                return self.command()
         else:
             print("Invalid command.")
             return self.command()
-
 w = warehouse()
 w.info()
 w.command()
